ea2b3ff5800d97fc2c36c23bc53f5bc07550a264
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-02-09 02:39:13 +0000

    initial commit

---
+++ ppc64le/mpich-configure.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CFLAGS="-fPIC -mcpu=powerpc64le" \
+ CXXFLAGS="-fPIC -mcpu=powerpc64le" \
+ FFLAGS=-fPIE \
+ CC=clang \
+ CXX=clang++ \
+ FC=flang \
+ F77=flang \
+ F18_FC=gfortran \
+ ./configure \
+  --prefix=/usr/local/mpich \
+  --with-device=ch3 2>&1 | tee configure.txt
---
+++ x86_64/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+SPACK_REF=a5440305173c218ec785b0d5a2dfa8b02bb0b731 # 2021-02-05
+MPICH_REF=v3.3.2 
+
+DOCKER_BUILDKIT=1 docker build \
+ -t gcr.io/ecp-e4s/ubuntu20.04-llvm-x86_64:experimental \
+ --build-arg SPACK_REF=${SPACK_REF} \
+ --build-arg MPICH_REF=${MPICH_REF} .
