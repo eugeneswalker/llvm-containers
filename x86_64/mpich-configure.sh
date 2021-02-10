6fd222f285d9499229788a1bb161dc1ff6f98f78
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-02-09 20:30:41 -0800

    fix mpich build (arch+rpath); put packages.yaml and compilers.yaml in user scope too

--- x86_64/mpich-configure.sh
+++ x86_64/mpich-configure.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-CFLAGS="-fPIC -mcpu=powerpc64le" \
- CXXFLAGS="-fPIC -mcpu=powerpc64le" \
+CFLAGS="-fPIC -march=x86-64" \
+ CXXFLAGS="-fPIC -march=x86-64" \
  FFLAGS=-fPIE \
  CC=clang \
  CXX=clang++ \
  FC=flang \
  F77=flang \
  F18_FC=gfortran \
+ LDFLAGS=-Wl,-rpath=/spack-llvm/view/lib \
  ./configure \
   --prefix=/usr/local/mpich \
   --with-device=ch3 2>&1 | tee configure.txt
