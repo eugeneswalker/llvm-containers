d21c664f68e9c2315666fbb04015f79ab49bf5f6
eugeneswalker <eugenesunsetwalker@gmail.com>
2021-02-10 05:39:28 +0000

    fix mpich build

--- ppc64le/mpich-configure.sh
+++ ppc64le/mpich-configure.sh
@@ -8,6 +8,7 @@ CFLAGS="-fPIC -mcpu=powerpc64le" \
  FC=flang \
  F77=flang \
  F18_FC=gfortran \
+ LDFLAGS=-Wl,-rpath=/spack-llvm/view/lib \
  ./configure \
   --prefix=/usr/local/mpich \
   --with-device=ch3 2>&1 | tee configure.txt
